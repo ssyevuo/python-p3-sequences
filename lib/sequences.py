b5585989aecb531f20a20eb9bde9028da1f078c4
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-02-12 20:50:27 +0300

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    '''Prints the Fibonacci sequence up to the given length.'''
+    sequence = []
+    a, b = 0, 1
+    while len(sequence) < length:
+        sequence.append(a)
+        a, b = b, a + b
+    print(sequence)
